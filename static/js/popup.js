a2d80e935b7616bb89a39bbbab56da8bdc325dd2
Mingyu Park <alsrb4298@naver.com>
2022-03-17 14:52:43 +0900

    Refactoring: remove old-lms and  manifest version up

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,8 +1,7 @@
-const item_list = ['lms', 'usaint', 'old-lms', 'notice', 'fun-system'];
+const item_list = ['lms', 'usaint', 'notice', 'fun-system'];
 const link = [
     'https://class.ssu.ac.kr/',
     'https://saint.ssu.ac.kr/',
-    'https://myclass.ssu.ac.kr/',
     'https://scatch.ssu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/',
     'https://fun.ssu.ac.kr/'
 ];
