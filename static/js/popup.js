d5c0b524085e990a54d002b53938609607d0f290
Mingyu Park <alsrb4298@naver.com>
2022-06-15 00:38:48 +0900

    [Update] update version 1.2

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,9 +1,11 @@
-const item_list = ['lms', 'usaint', 'notice', 'fun-system'];
+const item_list = ['lms', 'usaint', 'notice', 'fun-system', 'cse', 'oasis'];
 const link = [
     'https://class.ssu.ac.kr/',
     'https://saint.ssu.ac.kr/',
     'https://scatch.ssu.ac.kr/%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/',
-    'https://fun.ssu.ac.kr/'
+    'https://fun.ssu.ac.kr/',
+    'http://cse.ssu.ac.kr/main/index.htm',
+    'https://oasis.ssu.ac.kr/'
 ];
 document.addEventListener('click', function (e) {
     const clicked_ID = e.target.getAttribute('id');
